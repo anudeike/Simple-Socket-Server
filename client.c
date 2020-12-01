7e5e480b20e89ab3a48d2c781475a0ef439cc1f5
Ikechukwu <ikechukwuanude@gmail.com>
2020-12-01 19:22:52 +0000

    edited the client side code

--- client.c
+++ client.c
@@ -1,32 +1,88 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <sys/types.h> 
-#include <unistd.h>
-#include <string.h>
-#include <arpa/inet.h>
+#include"stdio.h"  
+#include"stdlib.h"  
+#include"sys/types.h"  
+#include"sys/socket.h"  
+#include"string.h"  
+#include"netinet/in.h"  
+#include"netdb.h"
+#include"pthread.h"
+  
+#define PORT 5000
+#define BUF_SIZE 2000 
+  
+void * receiveMessage(void * socket) {
+ int sockfd, ret;
 
+ char buffer[BUF_SIZE];
 
-// set up global variables
-struct sockaddr_in serv_socket;
-int file_descriptor; // ids the socket
-int connection; // ids the connection
+ sockfd = (int) socket;
 
-char message[100] = ""; // store messages sent to the server
+ memset(buffer, 0, BUF_SIZE); 
 
-int start(void){
-  file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
-  serv_socket.sin_family = AF_INET;
-  serv_socket.sin_port = htons(8096);
+ while(1) {
+  ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL);  
+  if (ret < 0) {  
+   printf("Could Not Recieve Data!\n");    
+  } else {
+   printf("server: ");
+   fputs(buffer, stdout);
+   //printf("\n");
+  }  
+ }
+}
 
-  inet_pton(AF_INET, "127.0.0.1", &serv_socket.sin_addr); //This binds the client to localhost
+int main(int argc, char**argv) {  
+ struct sockaddr_in addr, cl_addr;  
+ int sockfd, ret;  
+ char buffer[BUF_SIZE]; 
+ char * serverAddr;
+ pthread_t rThread;
 
-  connect(file_descriptor, (struct sockaddr *)&serv_socket, sizeof(serv_socket)); //This connects the client to the server.
-  while(1) {
-    printf("Enter a message: ");
-    fgets(message, 100, stdin);
-    send(file_descriptor, message, strlen(message), 0);
-    //An extra breaking condition can be added here (to terminate the while loop)
+ if (argc < 2) {
+  printf("usage: client < ip address >\n");
+  exit(1);  
+ }
+
+ serverAddr = argv[1]; 
+ 
+ sockfd = socket(AF_INET, SOCK_STREAM, 0);  
+ if (sockfd < 0) {  
+  printf("Could Not Create socket!\n");  
+  exit(1);  
+ }  
+ printf("Socket created...\n");   
+
+ memset(&addr, 0, sizeof(addr));  
+ addr.sin_family = AF_INET;  
+ addr.sin_addr.s_addr = inet_addr(serverAddr);
+ addr.sin_port = PORT;     
+
+ ret = connect(sockfd, (struct sockaddr *) &addr, sizeof(addr));  
+ if (ret < 0) {  
+  printf("Could not connect to server!\n");  
+  exit(1);  
+ }  
+ printf("Connected to the server...\n");  
+
+ memset(buffer, 0, BUF_SIZE);
+ printf("Enter your messages one at a time and press return!\n");
+
+ //creating a new thread for receiving messages from the server
+ ret = pthread_create(&rThread, NULL, receiveMessage, (void *) sockfd);
+ if (ret) {
+  printf("ERROR: Return Code from pthread_create() is %d\n", ret);
+  exit(1);
+ }
+
+ while (fgets(buffer, BUF_SIZE, stdin) != NULL) {
+  ret = sendto(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *) &addr, sizeof(addr));  
+  if (ret < 0) {  
+   printf("Problem with sending data...\n\t-%s", buffer);  
   }
+ }
+
+ close(sockfd);
+ pthread_exit(NULL);
+ 
+ return 0;    
 }
